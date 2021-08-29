976782e725408691a75ad8c4ac8431cc3ad44b32
Caio Emanuel <80735245+CaioemanuelIF@users.noreply.github.com>
2021-08-28 22:20:34 -0300

    first changes

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+/* scroll-to-top-btn*/
+window.addEventListener('scroll', function(){
+    var scroll = document.querySelector('.scrollTop')
+    scroll.classList.toggle("active", window.scrollY > 500)
+})
+
+function scrollToTop(){
+    window.scrollTo({
+        top: 0,
+        behavior:'smooth'
+    })
+}
+/*------------------------------------------*/
